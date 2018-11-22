3ffb71e652b110a32e1ad3775765eb23b75e6062
alexHooper <alexmohack@gmail.com>
2018-11-22 23:02:10 +0530

    create conversation model

--- message/models.py
+++ message/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	message = models.CharField(blank=True, null=True, max_length=256)
+	status = models.CharField(blank=True, null=True, max_length=256)
+	created_at = models.DateTimeField(auto_now=True)
