02d1e88e9d0791ec127075ca49dbc84259623825
alexHooper <alexmohack@gmail.com>
2018-11-23 15:40:27 +0530

    correct errors

--- customer/customer/settings.py
+++ customer/customer/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware'     # <- corsheaders
+    'corsheaders.middleware.CorsMiddleware',     # <- corsheaders
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
