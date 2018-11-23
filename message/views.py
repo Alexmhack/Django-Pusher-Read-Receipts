7f6bcfb1b0b9806ea4c11d445612309d2d144002
alexHooper <alexmohack@gmail.com>
2018-11-23 08:24:06 +0530

    add fetch and broadcast views

--- message/views.py
+++ message/views.py
@@ -12,3 +12,41 @@ pusher = Pusher(
 	key='b3687ab4-115b-40a1-aeaf-431e117cbb35',
 	secret='cBBp5PhCPL82Ff4mEuGYxwb0jaTdnSaZf/O7dK0gTgI=',
 	cluster='v1:us1:b3687ab4-115b-40a1-aeaf-431e117cbb35')
+
+@login_required()
+def index(request):
+	return render(request, 'chat.html')
+
+
+@csrf_exempt
+def broadcast(request):
+	con_msg = request.POST.get('message', '')
+	message = Conversation(message=con_msg, status='', user=request.user)
+	message.save()
+
+	pusher_msg = {
+		'name': message.user.username,
+		'status': message.status,
+		'message': message.message,
+		'id': message.id,
+		'created_at': message.created_at
+	}
+
+	# trigger the message channel and event to pusher
+	pusher.trigger('a_channel', 'an_event', pusher_msg)
+
+	# return json response of the broadcasted message
+	return JsonResponse(pusher_msg)
+
+def conversations(request):
+	data = Conversation.objects.all()
+	con_data = [{
+		'name': message.user.username,
+		'status': message.status,
+		'message': message.message,
+		'id': message.id,
+		'created_at': message.created_at
+	} for message in data]
+
+	# return json response of broadcasted messages
+	return JsonResponse(con_data)
