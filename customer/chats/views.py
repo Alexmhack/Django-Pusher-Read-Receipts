53906a7c6a62db0e33ce1de74e3a144e2d2eca07
alexHooper <alexmohack@gmail.com>
2018-11-23 14:21:07 +0530

    instantiate pusher chatkit

--- customer/chats/views.py
+++ customer/chats/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from pusher import Pusher
+from decouple import config
+
+pusher = Pusher(
+	app_id='2',
+	key=config('PUSHER_KEY'),
+	secret=config('PUSHER_SECRET'),
+	cluster=config('PUSHER_CLUSTER'),
+	ssl=True
+)
